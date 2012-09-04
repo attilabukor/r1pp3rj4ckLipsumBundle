336a8868ed4e5c95320c561b50ab2fcb1d2b5019
r1pp3rj4ck <attila.bukor@gmail.com>
2012-09-04 12:42:04 +0200

    added license for new files

--- Generator/RandomGeneratorInterface.php
+++ Generator/RandomGeneratorInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the LipsumBundle package.
+ *
+ * (c) Attila Bukor <attila.bukor@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace r1pp3rj4ck\LipsumBundle\Generator;
 
 interface RandomGeneratorInterface
